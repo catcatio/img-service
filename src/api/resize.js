e1ab16e9aead0ce87e02b0ac667cced483ca1aed
NiponChi <nipon.chi@gmail.com>
2018-08-11 23:31:36 +0700

    Set Content-Type

--- src/api/resize.js
+++ src/api/resize.js
@@ -20,15 +20,20 @@ const resizeApi = () => {
     const size = parseInt(req.query.size) || 512
     const quality = parseInt(req.query.quality) || 75
 
-    if ( !url ) {
+    if (!url) {
       const message = 'url is empty'
       console.error(reqId, message)
-      return res.status(400).send({message})
+      return res.status(400).send({ message })
     }
 
     console.log(reqId, '  resizing', url, size, quality)
     const resize = im().resize(`${size}x${size}`).quality(quality)
-    request.get(url).pipe(resize).pipe(res)
+    request.get(url)
+      .on('response', (response) => {
+        res.header('Content-Type', response.headers['content-type']);
+      })
+      .pipe(resize)
+      .pipe(res)
   }
 }
 
